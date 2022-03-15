f1fc0554b3577cee5493e5962acfbda748d3ba4f
EminElekberov <eminelekberov09@gmail.com>
2022-03-15 21:17:22 +0400

    update

--- LawyerbackEnd/LawyerbackEnd/Areas/Admin/Controllers/AccountController.cs
+++ LawyerbackEnd/LawyerbackEnd/Areas/Admin/Controllers/AccountController.cs
@@ -11,12 +11,26 @@ namespace LawyerbackEnd.Areas.Admin.Controllers
     [Area("Admin")]
     public class AccountController : Controller
     {
-        private readonly LawyerDbcontext dbcontext;
-        private readonly UserManager<User> userManager;
-        private readonly RoleManager<Ide>
+        private readonly LawyerDbcontext _dbcontext;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<User> _signInManager;
+        public AccountController(LawyerDbcontext portoDbContext, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, SignInManager<User> signInManager)
+        {
+            _dbcontext = portoDbContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            User user = new User();
+            return View(user);
         }
     }
 }
