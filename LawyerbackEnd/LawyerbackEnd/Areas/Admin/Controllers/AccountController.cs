fef6581ab8e87a25f90a376ff805badb3582c400
EminElekberov <eminelekberov09@gmail.com>
2022-03-15 19:36:17 +0400

    update

--- LawyerbackEnd/LawyerbackEnd/Areas/Admin/Controllers/AccountController.cs
+++ LawyerbackEnd/LawyerbackEnd/Areas/Admin/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LawyerbackEnd.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +11,9 @@ namespace LawyerbackEnd.Areas.Admin.Controllers
     [Area("Admin")]
     public class AccountController : Controller
     {
-
+        private readonly LawyerDbcontext dbcontext;
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<Ide>
         public IActionResult Index()
         {
             return View();
